afecd42140640685343b3d9e6dcbf95ab178ce4a
matheusAlvarenga <52459293+matheusAlvarenga@users.noreply.github.com>
2020-06-11 13:56:01 -0300

    Finished the Design of Database Editing Page

--- frontend/src/pages/individual_project/ip_body/index.js
+++ frontend/src/pages/individual_project/ip_body/index.js
@@ -20,7 +20,7 @@ import logoWpo from '../../../assets/logo_wpo.svg';
 
 export default function Project() {
 
-    const [selected, setSelected] = useState(2);
+    const [selected, setSelected] = useState(3);
 
     return (
         <div className="content-projs">
--- frontend/src/pages/individual_project/ip_database/index.js
+++ frontend/src/pages/individual_project/ip_database/index.js
@@ -1,16 +1,181 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { FiSmile } from 'react-icons/fi';
+import { FiDatabase, FiStar, FiEdit, FiPlus, FiChevronDown, FiSearch, FiFilePlus, FiSquare, FiMinusSquare, FiArrowRight, FiPlusSquare, FiCheckSquare, FiKey } from 'react-icons/fi';
 
 import './style.css';
 
-export default function To_Do_List() {
+export default function Database() {
 
     return (
     
-        <div className="to_do_lists_container">
+        <div className="database_container">
 
-            <h1 style={{color:'white'}}>Database</h1>
+            <div className="header">
+
+                <div className="left">
+
+                </div>
+
+                <div className="middle">
+
+                    <div className="title">
+
+                        <FiDatabase />
+
+                        <p>Database View</p>
+
+                    </div>
+
+                </div>
+
+                <div className="right">
+
+                    <div className="actions">
+
+                        <FiEdit />
+
+                    </div>
+
+                </div>
+
+            </div>
+
+            <div className="opt-list">
+
+                <div className="left">
+
+                    <p>All Tables</p>
+
+                    <FiChevronDown className="chv" />
+
+                </div>
+
+                <div className="right">
+
+                    <div className="head-item">
+
+                        <FiEdit />
+
+                        <p>Edit</p>
+
+                    </div>
+
+                    <div className="head-item">
+
+                        <FiSearch />
+
+                        <p>Search</p>
+
+                    </div>
+
+                    <div className="head-item">
+
+                        <FiPlus />
+
+                        <p>New Table</p>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+            <div className="list">
+
+                <div className="item">
+
+                    <div className="left">
+
+                        <p>Clientes</p>
+
+                    </div>
+
+                    <div className="right">
+
+                        <p>10 Columms</p>
+
+                        <FiMinusSquare className="pointer" />
+
+                    </div>
+
+                </div>
+
+                <div className="sub-list">
+
+                    <div className="sub-item primary">
+
+                        <div className="left">
+
+                            <FiKey />
+
+                            <p>Adm_id ( Primary Key )</p>
+
+                        </div>
+
+                        <div className="middle">
+
+                            <p>AUTO_INCREMENT</p>
+
+                        </div>
+
+                        <div className="right">
+
+                            <p>INT(11)</p>
+
+                        </div>
+
+                    </div>
+
+                    <div className="sub-item secundary">
+
+                        <div className="left">
+
+                            <FiKey />
+
+                            <p>Adm_id ( Secundary Key )</p>
+
+                        </div>
+
+                        <div className="middle">
+
+                            <p>AUTO_INCREMENT</p>
+
+                        </div>
+
+                        <div className="right">
+
+                            <p>INT(11)</p>
+
+                        </div>
+
+                    </div>
+
+                    <div className="sub-item nokey">
+
+                        <div className="left">
+
+                            <FiKey />
+
+                            <p>Adm_name</p>
+
+                        </div>
+
+                        <div className="middle">
+
+
+                        </div>
+
+                        <div className="right">
+
+                            <p>VARCHAR(60)</p>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
 
         </div>
 
