74c1a24c245e988d1fbc8f4747c6db893ee0165a
matheusAlvarenga <52459293+matheusAlvarenga@users.noreply.github.com>
2020-06-14 18:19:40 -0300

    Finished the design of Function Page

--- frontend/src/pages/individual_project/ip_body/index.js
+++ frontend/src/pages/individual_project/ip_body/index.js
@@ -20,7 +20,7 @@ import logoWpo from '../../../assets/logo_wpo.svg';
 
 export default function Project() {
 
-    const [selected, setSelected] = useState(4);
+    const [selected, setSelected] = useState(7);
 
     return (
         <div className="content-projs">
--- frontend/src/pages/individual_project/ip_function/index.js
+++ frontend/src/pages/individual_project/ip_function/index.js
@@ -1,16 +1,116 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { FiSmile } from 'react-icons/fi';
+import { FiLayout, FiEdit, FiStar, FiPlus, FiChevronDown, FiSearch, FiFilePlus, FiSquare, FiMinusSquare, FiArrowRight, FiPlusSquare, FiCheckSquare, FiKey } from 'react-icons/fi';
 
 import './style.css';
 
-export default function To_Do_List() {
+export default function Database() {
 
     return (
     
-        <div className="to_do_lists_container">
+        <div className="function_container">
 
-            <h1 style={{color:'white'}}>Function</h1>
+            <div className="header">
+
+                <div className="left">
+
+                    <div className="title">
+
+                        <FiLayout />
+
+                        <p>Function List</p>
+
+                        <p>/ Sobre Nós</p>
+
+                    </div>
+
+                </div>
+
+                <div className="right">
+
+                    <div className="actions">
+
+                        <FiEdit />
+
+                    </div>
+
+                </div>
+
+            </div>
+
+            <div className="opt-list">
+
+                <div className="left">
+
+                    <p>All Functions</p>
+
+                    <FiChevronDown className="chv" />
+
+                </div>
+
+                <div className="right">
+
+                    <div className="head-item">
+
+                        <FiEdit />
+
+                        <p>Edit</p>
+
+                    </div>
+
+                    <div className="head-item">
+
+                        <FiSearch />
+
+                        <p>Search</p>
+
+                    </div>
+
+                    <div className="head-item">
+
+                        <FiPlus />
+
+                        <p>New Function</p>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+            <div className="list">
+
+                <div className="item">
+
+                    <div className="left">
+
+                        <p>Header</p>
+
+                    </div>
+
+                    <div className="right">
+
+                        <p>DB: <span>Cliente</span></p>
+
+                        <p>Page: <span>Login</span></p>
+
+                        <FiMinusSquare className="pointer" />
+
+                    </div>
+
+                </div>
+
+                <div className="sub-list">
+
+                    <div className="desc">
+
+                        <p>Globally provide access to intermandated vortals via focused infomediaries. Efficiently streamline technically sound solutions and low-risk high-yield communities.</p>
+
+                    </div>
+
+                </div>
+
+            </div>
 
         </div>
 
