934ac84fe91bb7147a4055db81bab4fdd1831c78
matheusAlvarenga <52459293+matheusAlvarenga@users.noreply.github.com>
2020-06-14 19:19:57 -0300

    Finished the Design of Comments Page

--- frontend/src/pages/individual_project/ip_body/index.js
+++ frontend/src/pages/individual_project/ip_body/index.js
@@ -20,7 +20,7 @@ import logoWpo from '../../../assets/logo_wpo.svg';
 
 export default function Project() {
 
-    const [selected, setSelected] = useState(7);
+    const [selected, setSelected] = useState(5);
 
     return (
         <div className="content-projs">
--- frontend/src/pages/individual_project/ip_comments/index.js
+++ frontend/src/pages/individual_project/ip_comments/index.js
@@ -1,16 +1,178 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { FiSmile } from 'react-icons/fi';
+import { FiTrash2, FiCheckCircle } from 'react-icons/fi';
 
 import './style.css';
 
-export default function To_Do_List() {
+export default function Database() {
 
     return (
     
-        <div className="to_do_lists_container">
+        <div className="comments_container">
 
-            <h1 style={{color:'white'}}>Comments</h1>
+            <div className="header">
+
+                <div className="middle">
+
+                    <p>Owner's Comments</p>
+
+                </div>
+
+            </div>
+
+            <div className="list">
+
+                <div className="item">
+
+                    <div className="title">
+
+                        <div className="left">
+
+                            <p>Bruno Brigide</p>
+
+                            <p>/ Login Page</p>
+
+                        </div>
+
+                        <div className="right">
+
+                            <FiCheckCircle />
+
+                            <FiTrash2 />
+
+                        </div>
+
+                    </div>
+
+                    <div className="desc">
+
+                        <p>Synergistically extend high-quality value via premier deliverables. Enthusiastically seize web-enabled niches rather than process-centric results. Objectively morph cost effective users without.</p>
+
+                    </div>
+
+                </div>
+
+                <div className="item">
+
+                    <div className="title">
+
+                        <div className="left">
+
+                            <p>Bruno Brigide</p>
+
+                            <p>/ Login Page</p>
+
+                        </div>
+
+                        <div className="right">
+
+                            <FiCheckCircle />
+
+                            <FiTrash2 />
+
+                        </div>
+
+                    </div>
+
+                    <div className="desc">
+
+                        <p>Synergistically extend high-quality value via premier deliverables. Enthusiastically seize web-enabled niches rather than process-centric results. Objectively morph cost effective users without.</p>
+
+                    </div>
+
+                </div>
+
+                <div className="item">
+
+                    <div className="title">
+
+                        <div className="left">
+
+                            <p>Bruno Brigide</p>
+
+                            <p>/ Login Page</p>
+
+                        </div>
+
+                        <div className="right">
+
+                            <FiCheckCircle />
+
+                            <FiTrash2 />
+
+                        </div>
+
+                    </div>
+
+                    <div className="desc">
+
+                        <p>Synergistically extend high-quality value via premier deliverables. Enthusiastically seize web-enabled niches rather than process-centric results. Objectively morph cost effective users without.</p>
+
+                    </div>
+
+                </div>
+
+                <div className="item">
+
+                    <div className="title">
+
+                        <div className="left">
+
+                            <p>Bruno Brigide</p>
+
+                            <p>/ Login Page</p>
+
+                        </div>
+
+                        <div className="right">
+
+                            <FiCheckCircle />
+
+                            <FiTrash2 />
+
+                        </div>
+
+                    </div>
+
+                    <div className="desc">
+
+                        <p>Synergistically extend high-quality value via premier deliverables. Enthusiastically seize web-enabled niches rather than process-centric results. Objectively morph cost effective users without.</p>
+
+                    </div>
+
+                </div>
+
+                <div className="item">
+
+                    <div className="title">
+
+                        <div className="left">
+
+                            <p>Bruno Brigide</p>
+
+                            <p>/ Login Page</p>
+
+                        </div>
+
+                        <div className="right">
+
+                            <FiCheckCircle />
+
+                            <FiTrash2 />
+
+                        </div>
+
+                    </div>
+
+                    <div className="desc">
+
+                        <p>Synergistically extend high-quality value via premier deliverables. Enthusiastically seize web-enabled niches rather than process-centric results. Objectively morph cost effective users without.</p>
+
+                    </div>
+
+                </div>
+
+            </div>
 
         </div>
 
